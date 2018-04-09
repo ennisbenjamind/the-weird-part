260bacf44e170dc79fd5cb41a14b486d362422e0
Benjamin Ennis <ennis.benjamin.d@gmail.com>
2018-04-09 13:54:45 -0400

    the weird part

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      link: ""
+    }
+  }
+
+  getRandomInt() {
+    let min = Math.ceil(0);
+    let max = Math.floor(24);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  componentDidMount () {
+    fetch(`https://www.reddit.com/r/DeepIntoYouTube/new.json`, {
+      credentials: 'same-origin'
+    })
+    .then (response => {
+      if (response.ok) {
+        return response;
+      } else {
+        let errorMessage = `${response.status} (${response.statusText})`,
+        error = new Error(errorMessage);
+        throw(error);
+      }
+    })
+    .then(response => response.json())
+    .then(body => {
+      let link = body.data.children[this.getRandomInt()].data.url
+      this.setState({link: link})
+    })
+    .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1> THE WEIRD PART </h1>
+        <button><a target= "_blank" href= {this.state.link}>Get weird</a></button>
+      </div>
+    );
+  }
+}
+
+export default App;
